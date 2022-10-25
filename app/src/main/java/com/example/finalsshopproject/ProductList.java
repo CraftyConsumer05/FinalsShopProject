e4a0a43d85b5e772db40fde7c41aebd85790b450
irizzcodes <114680318+irizzcodes@users.noreply.github.com>
2022-10-25 17:15:01 +0800

    add productlist class

---
+++ app/src/main/java/com/example/finalsshopproject/ProductList.java
@@ -0,0 +1,14 @@
+package com.example.finalsshopproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductList extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.productlist);
+    }
+}
